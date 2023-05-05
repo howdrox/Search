fcf47ab436454182e708531dee77e247c702f296
Sun Jixiang <jixiang.sun@insa-lyon.fr>
2023-05-05 08:47:16 +0200

    byebye data maker

--- data_maker.py
+++
@@ -1,7 +0,0 @@
-import json
-
-a = [[False, True, True, False, False, False, False, False, False], [False, True, False, False, False, True, False, False, False], [False, False, False, False, False, False, False, False, False], [False, True, False, False, True, False, False, False, False], [False, False, True, False, False, False, False, False, False], [False, False, False, True, True, False, False, False, False], [False, False, False, False, True, False, False, False, False]]
-data = {"board": a}
-
-with open("data.json", "w") as f:
-    json.dump(data, f)
